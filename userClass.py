023941a3308714dc97b418243316d3d0cb840a49
David C. Li <sunnydavidli@gmail.com>
2023-04-27 11:25:01 +0800

    my first commit to create UserClass

---
+++ userClass.py
@@ -0,0 +1,15 @@
+# let's define a user Class
+
+class User:
+    # initializer 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # display info
+    # class method
+    def displayInfo(self):
+        print(self.name, self.age)
+
+u1 = User("David", 45)
+u1.displayInfo()
